cef32c8a307f21e52145a79f8201efd63c0ecb47
Janko <inikolovbiz@gmail.com>
2019-01-15 14:22:02 +0100

    second class added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+class InnerClass {
+    private innerMessage: string | undefined;
+    public provideMessage(): string {
+        this.innerMessage = 'Provided messsage!';
+        return this.innerMessage;
+    }
+}
+// tslint:disable-next-line:max-classes-per-file
 class Service {
+
     private message: string;
+
     constructor(message: string) {
         this.message = message;
     }
 
     public writeMessage() {
+
+        const innerObject = new InnerClass();
         // tslint:disable-next-line:no-console
-        console.log(this.message);
+        console.log(this.message + innerObject.provideMessage());
     }
 
 }
